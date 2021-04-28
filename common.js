abb922272bc35a70e0189c6b1d19fe9fc3b98ffe
TechTensta <60234827+TechTensta2021@users.noreply.github.com>
2021-04-28 18:18:30 +0200

    Add files via upload

--- common.js
+++ common.js
@@ -1,3 +1,7 @@
+
+const scriptTag = document.createElement('script');
+scriptTag.src='https://cdnjs.cloudflare.com/ajax/libs/blueimp-md5/2.18.0/js/md5.min.js';
+
 // Here you can change the name of the organization to update all pages
 var organization = "TechTensta";
 var title = "Tech Tensta - Hacker Challenge"
@@ -32,21 +36,27 @@ function includeTitle()
   document.title = title;
 }
 
-var hemligaOrdet='0100';
-var teckensnitt='stockholm type';
-var bakgrund='#fdbbd9';
-var text='#006ebf';
+var hemligaOrdet='db2f40f24260bc41db48d82d5e7abf1d';
+var teckensnitt='59c73c74b74f0940e5491723ddce698d';
+var bakgrund='c7a4e9535d83a84eb64119dd03919259';
+var text='7eac7bd4c5ae399d5118c5c09c67fa4d';
+
 
 function setAnswer(page,id){
+    document.head.appendChild(scriptTag);
     var answer = document.getElementById(id).value;
     checkAnswer(page,id,answer);
 }
 function setAnswer3(page, id1, id2,id3){
-    var answer1 = document.getElementById(id1).value;
-    var answer2 =document.getElementById(id2).value;
-    var answer3 = document.getElementById(id3).value;
+    document.head.appendChild(scriptTag);
+    var answer1 = document.getElementById(id1).value.toLowerCase();
+    var answer2 =document.getElementById(id2).value.toLowerCase();
+    var answer3 = document.getElementById(id3).value.toLowerCase();
+
     if (page ==2){
-      if (answer1.toLowerCase()==teckensnitt&&answer2.toLowerCase()==bakgrund&&answer3.toLowerCase()==text) {
+      if (md5(answer1)==teckensnitt&&md5(answer2)==bakgrund&&md5(answer3)==text) {
+        sessionStorage.setItem("a1",answer2);
+        sessionStorage.setItem("a3",answer3);
         setColor();
         box3.innerHTML="Korrekt!<br/>"+"Nästa <a href=\"sida3.html\">här</a>";
       }else
@@ -56,7 +66,7 @@ function setAnswer3(page, id1, id2,id3){
 
 function checkAnswer(page,id,answer){
   if (page==1){
-    if (answer==hemligaOrdet) 
+    if (md5(answer)==hemligaOrdet) 
         box2.innerHTML="Korrekt!<br/>"+"Nästa <a href=\"sida2.html\">här</a>";
     else
         box2.innerHTML="Fel, försök igen";
@@ -67,7 +77,10 @@ function checkAnswer(page,id,answer){
   }
 }
 function setColor(){
-  document.body.style.backgroundColor = bakgrund;
-  document.body.style.color=text;
+  //v1 = (typeof v1 !== 'undefined') ?  v1 : '#fdbbd9'
+  var v1=sessionStorage.getItem("a1");
+  var v2=sessionStorage.getItem("a2");
+  document.body.style.backgroundColor = v1;
+  document.body.style.color=v2;
 }
 //window.addEventListener("load",setButton,false);
