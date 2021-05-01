dc8f5a99613aee9937cdfb7348d8d07452b2d7f0
TechTensta <60234827+TechTensta2021@users.noreply.github.com>
2021-05-01 18:36:46 +0200

    Add files via upload

--- common.js
+++ common.js
@@ -36,11 +36,13 @@ function includeTitle()
   document.title = title;
 }
 
-var hemligaOrdet='db2f40f24260bc41db48d82d5e7abf1d';
+var hemligaOrdet='855f3f976217732f39584c61b922632f';
 var teckensnitt='59c73c74b74f0940e5491723ddce698d';
 var bakgrund='c7a4e9535d83a84eb64119dd03919259';
 var text='7eac7bd4c5ae399d5118c5c09c67fa4d';
-
+var lat='9b68edde3794c229ce5c492869e7fd03';
+var long='fc7daed4b11f52f768fd0059202188f5';
+var bokchiffer='302438cad422e1b278fd93da4373940b';
 
 function setAnswer(page,id){
     document.head.appendChild(scriptTag);
@@ -51,43 +53,56 @@ function setAnswer3(page, id1, id2,id3){
     document.head.appendChild(scriptTag);
     var answer1 = document.getElementById(id1).value.toLowerCase();
     var answer2 =document.getElementById(id2).value.toLowerCase();
-    var answer3 = document.getElementById(id3).value.toLowerCase();
 
-    if (page ==2){
+    if (page ==3){
+      var answer3 = document.getElementById(id3).value.toLowerCase();
       if (md5(answer1)==teckensnitt&&md5(answer2)==bakgrund&&md5(answer3)==text) {
         sessionStorage.setItem("a1",answer2);
         sessionStorage.setItem("a2",answer3);
         setColor();
-        box3.innerHTML="Korrekt!<br/>"+"Nästa <a href=\"sida3.html\">här</a>";
+        box3.innerHTML="Korrekt!<br/>"+"Nästa <a href=\"ctf.html\">här</a>";
       }else
         box3.innerHTML = "Fel, försök igen";
+
+    }else if(page==6){
+      if(md5(answer1)==lat && md5(answer2)==long)
+        box7.innerHTML="Korrekt!<br/>"+"Nästa <a href=\"bokchiffer.html\">här</a>";
+      else
+        box7.innerHTML = "Fel, försök igen";
     }
 }
 
 function checkAnswer(page,id,answer){
   if (page==1){
     if (md5(answer)==hemligaOrdet) 
-        box2.innerHTML="Korrekt!<br/>"+"Nästa <a href=\"sida2.html\">här</a>";
+        box2.innerHTML="Korrekt!<br/>"+"Nästa <a href=\"ifelse.html\">här</a>";
     else
         box2.innerHTML="Fel, försök igen";
 
-  }else if(page==3){
-    if (answer >=10) box4.innerHTML="Korrekt!<br/> Mycket bra jobbat!"
-    +"Nästa <a href=\"sida4.html\">här</a>";
-    else box4.innerHTML="Fel svar, försök igen";
+  }else if(page==2){
+    if (answer >=10) 
+      box4.innerHTML="Korrekt!<br/> Mycket bra jobbat!"
+      +"Nästa <a href=\"ssgp.html\">här</a>";
+    else 
+      box4.innerHTML="Fel svar, försök igen";
+
   }else if(page==4){
-    if(answer=='flag{PR7-R8z-kGK-Vrm}'|| answer=='{PR7-R8z-kGK-Vrm}'||answer=='PR7-R8z-kGK-Vrm'){
-      box5.innerHTML="Korrekt!<br/> Mycket bra jobbat!"
-    +"Nästa <a href=\"sida5.html\">här</a>";
-    }else{
+    if(answer=='flag{PR7-R8z-kGK-Vrm}'|| answer=='{PR7-R8z-kGK-Vrm}'||answer=='PR7-R8z-kGK-Vrm')
+      box5.innerHTML="Korrekt!<br/> Mycket bra jobbat!"+"Nästa <a href=\"latLong.html\">här</a>";
+    else
       box5.innerHTML="Fel svar, försök igen";
-    }
+
   }else if(page ==5){
-    if (answer.toLowerCase= "alla kan koda"||answer=='flag{Alla-Kan-Koda}'){
-      box6.innerHTML="Korrekt!<br/> Mycket bra jobbat!"
-    }else{
+    if (answer.toLowerCase== "alla-kan-koda"||answer=='flag{Alla-Kan-Koda}')
+      box6.innerHTML="Korrekt!<br/> Mycket bra jobbat!"+"Nästa <a href=\"quickResponse.html\">här</a>";
+    else
       box6.innerHTML="Fel svar, försök igen";
-    }
+
+  }else if(page==7){
+    if(md5(answer.toLowerCase())==bokchiffer)
+      box8.innerHTML="Korrekt!<br/> Mycket bra jobbat!";
+    else
+      box8.innerHTML="Fel,försök igen";
   }
 }
 function setColor(){
